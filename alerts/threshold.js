f77066abf382247ae7c71384bdd9f2c1c520d911
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-08-07 15:39:25 +1000

    Clean up threshold logic

--- alerts/threshold.js
+++ alerts/threshold.js
@@ -1,6 +1,14 @@
 var lowpassEdges = require('../lib/lowpassEdges');
 
-module.exports = function(type, properties, opts) {
+module.exports = function(type, property, opts) {
+  if (!type || !property) {
+    throw new Error('threshold needs a type and property/ies!');
+  }
+
+  var properties = typeof property === 'string'
+    ? [property]
+    : (property || []);
+
   // 'Member' variables for this particular alerter. Note that one alerter might
   // be 'instantiated' multiple times by being added in different ways, so these
   // one-time variables should only be things that relate to the input arguments.
@@ -8,12 +16,6 @@ module.exports = function(type, properties, opts) {
   var period = opts.period;
   var threshold = opts.threshold;
   var doFirstUpdate = opts.alwaysAlertOnFirstReport;
-
-  // We accept a single property name or a list of them. In the case of the
-  // former, convert it to a list.
-  if (typeof properties === 'string') {
-    properties = [properties];
-  }
 
   function belowThreshold(v) {
     return v < threshold;
@@ -30,16 +32,9 @@ module.exports = function(type, properties, opts) {
     return value.low;
   }
 
-  // Called once when we are added to an alerter.
   function init(context, emit) {
-    // We need to store this so we can emit unconditionally on the first update.
-    context.emit = emit;
-    context.firstUpdate = true;
-
-    // Because we may be listening to updates from many peers, we'll threshold
-    // across all of them. We need to keep a 'working memory' of the state of
-    // each peer (i.e. whether they were below the threshold last update).
     context.peerStates = {};
+    context.firstUpdate = true;
 
     // This is expected to be called frequently, but only call its callback upon
     // significant events.
@@ -48,12 +43,12 @@ module.exports = function(type, properties, opts) {
     }, period);
   }
 
-  // When we receive a report, analyse it for the conditions we care about.
-  function onStatsReport(report, reporter, context) {
+  function onStatsReport(report, reporter, context, emit) {
     if (!context.peerStates[reporter.target]) {
       context.peerStates[reporter.target] = {low: false};
     }
     var state = context.peerStates[reporter.target];
+    state.lastUpdate = +(new Date());
 
     var values = properties.map(function(prop) { return report.data[prop]; });
     var anyLow = values.some(belowThreshold);
@@ -67,10 +62,9 @@ module.exports = function(type, properties, opts) {
     // we'll fire an event immediately. This is necessary because the lowpassed
     // signal output doesn't fire on the first event.
     if (context.firstUpdate && doFirstUpdate && state.low) {
-      context.emit(makeWarning(state.low));
+      emit(makeWarning(state.low));
       context.firstUpdate = false;
     }
-    state.lastUpdate = +(new Date());
 
     emitEventIfAnyPeersLow(context);
   }
@@ -79,7 +73,7 @@ module.exports = function(type, properties, opts) {
   // if it could be inlined into the only place it's used.
   function emitEventIfAnyPeersLow(context) {
     var states = Object.keys(context.peerStates).map(getPeerState);
-    var recentStates = states.filter(withinTimeWindow);
+    var recentStates = states.filter(recent);
     var anyStatesLow = recentStates.some(getWarning);
     context.outputSignal(anyStatesLow);
 
@@ -88,10 +82,7 @@ module.exports = function(type, properties, opts) {
     }
   }
 
-  // In order to prevent dropped connections with low bandwidth from clogging
-  // up future monitoring, we discard states that haven't been updated for a
-  // while. Where 'a while' is subjective.
-  function withinTimeWindow(value) {
+  function recent(value) {
     var now = +(new Date());
     return (now - value.lastUpdate) < 2*period;
   }
