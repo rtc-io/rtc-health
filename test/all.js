85c594375da169d6e38e33cf8c18c79ea6048ca0
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-09-02 16:45:09 +1000

    Add tests for failure tracking

--- test/all.js
+++ test/all.js
@@ -1,2 +1,3 @@
 require('./health-test')(location.origin);
 require('./alerts-test')(location.origin);
+require('./failure-test')(location.origin);
---
+++ test/failure-test.js
@@ -0,0 +1,54 @@
+var test = require('tape');
+var async = require('async');
+var peerHelper = require('./helpers/peer');
+var room = 'rtchealth-ut-' + require('uuid').v4();
+var Promise = require('es6-promise').Promise;
+
+// require('cog/logger').enable('*');
+module.exports = function(signallingServer) {
+
+    var newPeer = peerHelper.peerCreator(signallingServer, {
+        room: room,
+        filterCandidate: function() {
+          return false;
+        },
+        monitorOpts: {
+            pollInterval: 10000,
+            connectionFailureTime: 100,
+        },
+    });
+
+    test('rtc-health events', function(t) {
+
+        var createPeer = newPeer.bind(newPeer, t);
+        async.parallel([createPeer, createPeer], function(err, peers) {
+                
+            t.ok(peers.length === 2, 'two peers created');
+
+            var source = peers[0];
+            var target = peers[1];
+
+            var firstSourceFailure = new Promise(function(resolve, reject) {
+                // We fulfil the promise only if the connection failure is noticed.
+                source.monitor.on('health:connection:failure', function(tc) {
+                    resolve(tc);
+                });
+
+                // Timeout to keep the test from stalling.
+                setTimeout(function() {
+                    reject();
+                }, 1000);
+            });
+
+            t.test('track connection failure', function(t) {
+                firstSourceFailure.then(function() {
+                    t.pass('connection failure was caught');
+                    t.end();
+                }, function() {
+                    t.fail('connection failure was not caught');
+                    t.end();
+                });
+            });
+        });
+    });
+};
