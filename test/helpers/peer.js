25cdbfa4517f070368c0a4d6d4fe3e6f9933c4de
Nathan Oehlman <nathan.oehlman@gmail.com>
2015-12-01 11:09:56 +1000

    Remove unnecessary logging

--- test/helpers/peer.js
+++ test/helpers/peer.js
@@ -6,7 +6,6 @@ exports.peerCreator = function(signallingServer, opts) {
 	opts = opts || {};
 	return function(t, callback) {
 	    t.test('create a new peer', function(t) {
-	    	console.log('create peer');
 	        t.plan(3);
 	        var sig = signaller(require('rtc-switchboard-messenger')(signallingServer));
 	        sig.once('connected', function() {
